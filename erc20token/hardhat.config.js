9bdfc0c30b96431f7618a1b618e7c9e1b79a149a
Ezhil2k <ezhil17vr@gmail.com>
2024-01-05 00:15:13 +0530

    not deploying so deployed in remix

--- erc20token/hardhat.config.js
+++ erc20token/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const sepoliaUrl =  process.env.SEPOLIA_INFURA_ENDPOINT;
+const privateKey = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.20",
+//   settings: {
+//     optimizer: {
+//       enabled: true,
+//       runs: 1000,
+//     },
+//   },
+// allowUnlimitedContractSize: true,
+  networks: {
+    sepolia: {
+      url: sepoliaUrl,
+      accounts: [privateKey],
+      chainId: 11155111
+    }
+  }
 };
